bc059c7678f0ecec5b42c5935965b521036fe691
vinomca-megam <vino.v@megam.io>
2018-03-14 17:30:09 +0530

    .gitingore added

--- target/debug/deps/rioos_entitlement-f07796dc561c9cec.d
+++
@@ -1,5 +0,0 @@
-/home/vinov/code/rioo/nalperion_rust/target/debug/deps/librioos_entitlement-f07796dc561c9cec.rlib: src/lib.rs
-
-/home/vinov/code/rioo/nalperion_rust/target/debug/deps/rioos_entitlement-f07796dc561c9cec.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/librioos_entitlement.d
+++
@@ -1,1 +0,0 @@
-/home/vinov/code/rioo/nalperion_rust/target/debug/librioos_entitlement.rlib: /home/vinov/code/rioo/nalperion_rust/src/lib.rs
